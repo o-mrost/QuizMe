1307fce22f73786ade8caf97df38d2218fbc8fe3
O.Mrost <olgamrost@gmail.com>
2016-12-04 17:02:10 +0100

    Added two pages: one with four quizzez to choose from, and the other one with four answers. No logic, just layout

--- app/src/main/java/quizme/olgamrost/com/quizme/MainActivity.java
+++ app/src/main/java/quizme/olgamrost/com/quizme/MainActivity.java
@@ -1,7 +1,9 @@
 package quizme.olgamrost.com.quizme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showQuestion (final View cmd){
+
+        final Intent intent = new Intent(this, ShowQuestionsPage.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/quizme/olgamrost/com/quizme/ShowQuestionsPage.java
@@ -0,0 +1,16 @@
+package quizme.olgamrost.com.quizme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by olgamrost on 04/12/2016.
+ */
+public class ShowQuestionsPage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.showquestion);
+    }
+}
