388716728c3349f7cc19ae8c81ce2b5824f3f7dc
KONZMIIX700\konz <brose.konstantin@googlemail.com>
2016-12-07 15:39:35 +0100

    irgendetwas

--- app/src/main/java/quizme/olgamrost/com/quizme/MainActivity.java
+++ app/src/main/java/quizme/olgamrost/com/quizme/MainActivity.java
@@ -1,15 +1,22 @@
 package quizme.olgamrost.com.quizme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);}
 
-        //Hallo
+    public void onClickMenu (final View cmd)
+    {
+
+        startActivity(new Intent(this, MenuMain.class));
+
+//hallo
     }
 }
