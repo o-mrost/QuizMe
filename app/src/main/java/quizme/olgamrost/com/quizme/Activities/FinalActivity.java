cb412b9b39ab670edf1be3af860967a388db9a1a
o-mrost <olgamrost@gmail.com>
2016-12-25 16:22:31 +0100

    Feature/olgas ui suggestion (#1)
    * Added two pages: one with four quizzez to choose from, and the other one with four answers. No logic, just layout
    
    * Added activity to open a new window to check the answer and go to the next question
    
    * Deserialised JSON file (saved in assets folder), displayed question and answers on the buttons and passed the question to the new page to validate the answer
    
    * Tried to count the number of questions already answered. Without much success for now
    
    * Answers are shown on buttons, button listeners to show correct and false antworts
    
    * disable Buttons
    
    * Added new activity to show several questions from json
    
    * Added randomizing functionality for answer and solution
    
    * Questions can be shown one after the other
    
    * Added counting the correct questions feature and showing the score. Cleaned up old files, deleted activities to try the new features, left only the latest one.

---
+++ app/src/main/java/quizme/olgamrost/com/quizme/Activities/FinalActivity.java
@@ -0,0 +1,33 @@
+package quizme.olgamrost.com.quizme.Activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import quizme.olgamrost.com.quizme.R;
+
+/**
+ * Created by olgamrost on 22/12/2016.
+ */
+
+public class FinalActivity extends Activity {
+
+    double correctAnswersInt, totalQuestions;
+
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_final);
+
+        Intent intent = getIntent();
+        correctAnswersInt = intent.getIntExtra("correctAnswers", 0);
+        totalQuestions = intent.getIntExtra("totalQuestions", 0);
+
+        TextView correctAnswers = (TextView) findViewById(R.id.correctAnswers);
+        TextView totalResult = (TextView) findViewById(R.id.totalResult);
+
+        double score = correctAnswersInt / totalQuestions * 100;
+
+        correctAnswers.setText("You answered correctly " + (int) correctAnswersInt + " questions");
+        totalResult.setText("Your score is " + score + "%");
+    }
+}
---
+++ app/src/main/java/quizme/olgamrost/com/quizme/Json/AnswersBean.java
@@ -0,0 +1,44 @@
+package quizme.olgamrost.com.quizme.Json;
+
+/**
+ * Created by olgamrost on 20/12/2016.
+ */
+
+
+    public  class AnswersBean {
+        /**
+         * answer : Transport Layer
+         * solution : true
+         * _id : 5844744155b10b3d26489773
+         */
+
+        private String answer;
+        private boolean solution;
+        private String _id;
+
+        public String getAnswer() {
+            return answer;
+        }
+
+        public void setAnswer(String answer) {
+            this.answer = answer;
+        }
+
+        public boolean isSolution() {
+            return solution;
+        }
+
+        public void setSolution(boolean solution) {
+            this.solution = solution;
+        }
+
+        public String get_id() {
+            return _id;
+        }
+
+        public void set_id(String _id) {
+            this._id = _id;
+        }
+    }
+
+
