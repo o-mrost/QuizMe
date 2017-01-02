ba6c3a932cc933ad80ab08721d1147780252edab
Max Michalek <michalek.info@gmail.com>
2017-01-02 19:31:23 +0100

    Updated Server from testing to productive

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-/**
- * Created by Max on 02.12.16.
- */
+
 
 var express = require('express');
 var path = require('path');
@@ -11,10 +9,10 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 var azureUrl = process.env.CONNECTION_STRING;
-//var url = 'mongodb://localhost:27017/QuizApp';
+var url = 'mongodb://localhost:27017/QuizApp';
 //var url = 'mongodb://quizzappdb:dg93krkdqV5hKx5xkcv0sOICL6k3hoDsvZXjaQQZXyABs7BJpkuasf3HmRsqUSczokdyy0MFWLzkgjhyQY1Osw==@quizzappdb.documents.azure.com:10250/?ssl=true'
-mongoose.connect(azureUrl);
-//mongoose.connect(url);
+
+mongoose.connect(url);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
@@ -23,57 +21,66 @@ db.once('open', function () {
 });
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var users = require('./routes/user');
 var networkRouter = require('./routes/networkRouter');
 
 var app = express();
 
+var env = process.env.NODE_ENV || 'development';
+app.locals.ENV = env;
+app.locals.ENV_DEVELOPMENT = env == 'development';
+
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
+// app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(cookieParser());
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
 app.use('/networks',networkRouter);
 
-// catch 404 and forward to error handler
+/// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handlers
+/// error handlers
+
 // development error handler
 // will print stacktrace
+
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err,
+            title: 'error'
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {},
+        title: 'error'
+    });
 });
+
 
 module.exports = app;
