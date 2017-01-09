2c24c8edb578cd1481b62608137f5c70152822e2
O.Mrost <olgamrost@gmail.com>
2017-01-09 13:04:49 +0100

    Added get request to the server, tried to save the file as txt.

--- app/src/main/java/quiz/olgamrost/com/quiz/MainActivity.java
+++ app/src/main/java/quiz/olgamrost/com/quiz/MainActivity.java
@@ -2,6 +2,8 @@ package quiz.olgamrost.com.quiz;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -11,6 +13,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -22,6 +41,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         numberOfQuestionsAnswered = 0;
         numberOfCorrectAnswers = 0;
@@ -91,6 +111,103 @@ public class MainActivity extends AppCompatActivity
             showQuestions(getCurrentFocus());
 
         } else if (id == R.id.second_quiz) {
+
+            // get json
+            RequestQueue queue = Volley.newRequestQueue(this);
+
+            final String response1 = "error";
+            final String url = "http://httpbin.org/get?param1=hello";
+
+            // prepare the Request
+            JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+
+                            // display response
+                            Log.d("Response", response.toString());
+
+
+                            String fileName="myfile.txt";
+                            String input="Hello World";
+                            String path = Environment.getExternalStorageState();
+                            File file=null;
+                            if (Environment.MEDIA_MOUNTED.equals(path)) {
+                                try {
+                                    byte[] attachment = input.getBytes();
+
+                                    File folder = new File(Environment.getExternalStoragePublicDirectory(
+                                            Environment.DIRECTORY_DOWNLOADS)+"/Email_Client/");
+                                    folder.mkdirs();
+
+
+                                    file=new File(folder,fileName);
+                                    //Automatically creates the new empty file specified by the name,   if it is not exist.
+                                    file.createNewFile();
+                                    Log.i("EmailClient", "saveFile: Dir created");
+                                    FileOutputStream out = new FileOutputStream(file);
+                                    out.write(attachment);
+
+                                    out.close();
+
+                                    Log.v ("End of new ", " attempt");
+
+                                } catch (IOException e) {
+                                    Log.e("EmailClient", "saveFile: File not saved", e);;
+                                }
+                            }
+
+//                            try {
+//                                String externalStorageState = Environment.getExternalStorageState();
+//                                Log.v("storage state: ", externalStorageState);
+//
+////                                File root = new File(Environment.getExternalStorageDirectory(), "Notes");
+////                                if (!root.exists()) {
+////                                    root.mkdirs();
+////                                }
+//
+//                                File folder = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/EmailClient/");
+//                                folder.mkdirs();
+//
+//                                Log.v("Folder", folder.toString());
+//
+//                                String sFileName = "sixthQuiz.txt";
+//
+//                                File file = new File(folder, sFileName);
+//                                file.createNewFile();
+//
+//                                FileWriter writer = new FileWriter(file);
+//
+//                                writer.append(response.toString());
+//                                writer.flush();
+//                                writer.close();
+//
+////                                Writer output = null;
+////                                File file = new File("storage/sdcard/MyIdea/MyCompositions/" + "sixthQuiz" + ".txt");
+////                                output = new BufferedWriter(new FileWriter(file));
+////                                output.write(response.toString());
+////                                output.close();
+//                                Toast.makeText(getApplicationContext(), "File saved", Toast.LENGTH_LONG).show();
+//
+//                            } catch (Exception e) {
+//                                Log.v("Exception: ", e.getMessage());
+//                                Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+//                                e.printStackTrace();
+//                            }
+//                            finish();
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.d("Error.Response", response1);
+                        }
+                    }
+            );
+
+            // add it to the RequestQueue
+            queue.add(getRequest);
+
             fileAddress = "secondQuiz.txt";
             showQuestions(getCurrentFocus());
 
--- app/src/main/java/quiz/olgamrost/com/quiz/ResponseRepository.java
+++ app/src/main/java/quiz/olgamrost/com/quiz/ResponseRepository.java
@@ -1,7 +1,13 @@
 package quiz.olgamrost.com.quiz;
 
 import android.content.res.AssetManager;
+import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -10,6 +16,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
+import java.net.URI;
 import java.util.Collection;
 import java.util.List;
 
@@ -24,10 +31,37 @@ public class ResponseRepository {
 
     private AssetManager assetManager;
 
-    public ResponseRepository(AssetManager assetManager)
-    {
+    public ResponseRepository(AssetManager assetManager) {
         this.assetManager = assetManager;
     }
+
+//    public List<Response> getResponcesOnline() {
+//
+//        String url = "quizmerbn.azurewebsites.net/networks";
+//
+//        // Instantiate the RequestQueue.
+//        RequestQueue queue = Volley.newRequestQueue(this);
+////        String url = "http://www.google.com";
+//
+//// Request a string response from the provided URL.
+//        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+//                new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String response) {
+//                        // Display the first 500 characters of the response string.
+//                        Log.v("", "Response is: " + response.substring(0, 500));
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                Log.v("", "That didn't work!");
+//            }
+//        });
+//// Add the request to the RequestQueue.
+//        queue.add(stringRequest);
+//
+//        return null;
+//    }
 
     public List<Response> GetResponses(String fileAddress) {
 
@@ -47,7 +81,8 @@ public class ResponseRepository {
             Gson gson = new Gson();
 
             // http://stackoverflow.com/questions/9598707/gson-throwing-expected-begin-object-but-was-begin-array
-            Type collectionType = new TypeToken<Collection<Response>>(){}.getType();
+            Type collectionType = new TypeToken<Collection<Response>>() {
+            }.getType();
             Collection<Response> items = gson.fromJson(responcestr, collectionType);
 
             List<Response> list = (List<Response>) items;
