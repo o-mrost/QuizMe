9c3500cfcce2a91aa3416db924e6397ea5fc7b09
KONZMIIX700\konz <brose.konstantin@googlemail.com>
2017-01-09 12:59:36 +0100

    added help xml

--- app/src/main/java/quiz/olgamrost/com/quiz/ShowHelp.java
+++ app/src/main/java/quiz/olgamrost/com/quiz/ShowHelp.java
@@ -4,14 +4,44 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static javax.xml.transform.OutputKeys.ENCODING;
 
 public class ShowHelp extends Activity {
-
+    private static final String MIMETYPE = "text/html";
+    private static final String ENCODING = "UTF-8";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_show_help);
+        super.onCreate( savedInstanceState );
+
+        setContentView( R.layout.helpview );
+
+        final WebView view = (WebView) findViewById( R.id.helpview );
+        view.getSettings().setJavaScriptEnabled( true );
+
+
+
+        InputStream is = getResources().openRawResource( R.raw.help );
+
+        try {
+            if (( is != null ) && is.available() > 0) {
+                final byte[] bytes = new byte[is.available()];
+                is.read( bytes );
+                view.loadDataWithBaseURL( null, new String( bytes ), MIMETYPE,
+                        ENCODING, null );
+
+            }
+        } catch ( IOException ex ) {
+            ex.printStackTrace();
+        }
+
+        view.bringToFront();
     }
+
 
     protected void goBack(View v){
 
